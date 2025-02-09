6a38aa3f8ad5d056590c4bd02fb1ba0a5b958f82
leeturner <lee@leeturner.me>
2025-02-09 15:07:41 +0000

    Initial commit of the cli with generated code from micronaut launch

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.kapt") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.4.4"
+}
+
+version = "0.1"
+group = "com.leeturner"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    kapt("info.picocli:picocli-codegen")
+    kapt("io.micronaut.serde:micronaut-serde-processor")
+    implementation("info.picocli:picocli")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.picocli:micronaut-picocli")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+
+application {
+    mainClass = "com.leeturner.cli.XtomdCommand"
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+
+micronaut {
+    testRuntime("kotest5")
+    processing {
+        incremental(true)
+        annotations("com.leeturner.*")
+    }
+}
+
+
+tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
+    jdkVersion = "21"
+}
+
+
---
+++ src/main/kotlin/com/leeturner/xtomd/cli/XtomdCommand.kt
@@ -0,0 +1,25 @@
+package com.leeturner.xtomd.cli
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+
+@Command(name = "xtomd", description = ["..."], mixinStandardHelpOptions = true)
+class XtomdCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+          PicocliRunner.run(XtomdCommand::class.java, *args)
+        }
+    }
+}
