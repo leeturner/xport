4643be9e6e6f8ec0ef06a6a4bcdd40387a61e1f5
leeturner <lee@leeturner.me>
2025-02-09 15:36:51 +0000

    Update to the latest kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.kotlin.kapt") version "1.9.25"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    id("org.jetbrains.kotlin.kapt") version "2.1.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.1.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.4.4"
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.25"
+    id("org.jetbrains.kotlin.jvm") version "2.1.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.1.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
