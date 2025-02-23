664d4e1a41c84e89cca18610d08d2c65440a1f9f
leeturner <lee@leeturner.me>
2025-02-23 17:36:00 +0000

    Delete file that is no longer needed

--- src/main/kotlin/com/leeturner/xport/domain/pipeline/workers/NormaliseTweetJsFileWorker.kt
+++
@@ -1,9 +0,0 @@
-package com.leeturner.xport.domain.pipeline.workers
-
-import com.leeturner.xport.domain.pipeline.Context
-
-class NormaliseTweetJsFileWorker : Worker {
-    override fun run(context: Context) {
-        TODO("Not yet implemented")
-    }
-}
