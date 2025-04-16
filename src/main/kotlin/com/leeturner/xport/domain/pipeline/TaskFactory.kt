b41f341496e0f3ff4f3a9a74433f68d8fc5b4045
leeturner <lee@leeturner.me>
2025-04-16 16:50:36 +0100

    Tasks are now managed by micronaut beans
    We now have a micronaut factory that manages the tasks passed to the
    pipeline

--- src/main/kotlin/com/leeturner/xport/domain/pipeline/TaskFactory.kt
+++ src/main/kotlin/com/leeturner/xport/domain/pipeline/TaskFactory.kt
@@ -1,4 +1,24 @@
 package com.leeturner.xport.domain.pipeline
 
+import com.leeturner.xport.domain.pipeline.tasks.ConvertTweetJsToJsonTask
+import com.leeturner.xport.domain.pipeline.tasks.ConvertTweetJsonToMarkdownTask
+import com.leeturner.xport.domain.pipeline.tasks.CopyTweetJsFileTask
+import com.leeturner.xport.domain.pipeline.tasks.CopyTweetsMediaFolderTask
+import com.leeturner.xport.domain.pipeline.tasks.DeleteTweetJsFileTask
+import com.leeturner.xport.domain.pipeline.tasks.Task
+import io.micronaut.context.annotation.Factory
+import io.micronaut.serde.ObjectMapper
+import jakarta.inject.Singleton
+
+@Factory
 class TaskFactory {
+    @Singleton
+    fun tasks(objectMapper: ObjectMapper): List<Task> =
+        listOf(
+            CopyTweetJsFileTask(),
+            ConvertTweetJsToJsonTask(),
+            ConvertTweetJsonToMarkdownTask(objectMapper),
+            CopyTweetsMediaFolderTask(),
+            DeleteTweetJsFileTask(),
+        )
 }
